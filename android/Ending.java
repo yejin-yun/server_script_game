8cef0b938701e61257f7544a38acaf424ad6cb80
yejin-yun <59862820+yejin-yun@users.noreply.github.com>
2020-02-19 23:22:32 +0900

    server_count_1

---
+++ android/Ending.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+public class Ending {
+	int id;
+	String title;
+	int cnt;
+	String url;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
---
+++ android/EndingActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+
+public class EndingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ending);
+
+        TextView textView3= (TextView)findViewById(R.id.textView3);
+        Intent intent = getIntent();
+        int cnt = intent.getExtras().getInt("cnt");
+        Log.d(this.getClass().getName(), (String)textView3.getText());
+        textView3.setText("동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "남산 위에 저 소나무 철갑을 두른 듯 바람서리 불변함은 우리 기상일세\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "가을 하늘 공활한데 높고 구름 없이 밝은 달은 우리 가슴 일편담심일세.\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라 사랑하세.\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "남산 위에 저 소나무 철갑을 두른 듯 바람서리 불변함은 우리 기상일세\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "가을 하늘 공활한데 높고 구름 없이 밝은 달은 우리 가슴 일편담심일세.\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세\n" +
+                "이 기상과 이 맘으로 충성을 다하여 괴로우나 즐거우나 나라 사랑하세.\n" +
+                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세"
+                + "\n\n\n\n\n 해당 엔딩은 총" + cnt + "명이 본 엔딩입니다.\n"
+        );
+        textView3.setMovementMethod(new ScrollingMovementMethod());
+    }
+}
